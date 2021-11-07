f294da1c962c8c3b9ca07ce10f9d9ac2e674100b
Anthony Chen <50970130+achen318@users.noreply.github.com>
2021-11-07 13:25:27 -0500

    Revert "Update about.js"
    This reverts commit df28ab6e2905e7f5dbcdb2932c903b24cc0486b6.

--- pages/about.js
+++ pages/about.js
@@ -5,26 +5,6 @@ export default function About() {
     <Layout title="About Us">
       <h1>About Us</h1>
       <p>h</p>
-      <h1>About Us<br></h1>
-
-      <h4>Anthony Chen</h4>
-      <ol>
-        <li>Middleware</li>
-        <li>Back End</li>
-      </ol>
-      <h4><br>Sudipta Chakraborty</h4>
-      <ol>
-        <li>Front End</li>
-        <li>Middleware</li>
-      </ol>
-      <h4><br>Shaurya Sen<br></h4>
-      <ol>
-        <li>Front End</li>
-      </ol>
-      <h4><br>Jack Fang<br></h4>
-      <ol>
-        <li>Front End</li>
-      </ol>
     </Layout>
   );
 }
