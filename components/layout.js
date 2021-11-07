1304a9af81c0e7d3370bd6ce1d0eb4072990fdff
Anthony Chen <50970130+achen318@users.noreply.github.com>
2021-11-07 16:41:19 -0500

    Steal landing page from main branch

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,8 @@
 import Script from 'next/script';
 import { useUser } from '@auth0/nextjs-auth0';
 
+import Image from 'next/image';
+
 import Head_ from './head_';
 import Menu from './menu';
 import Footer from './footer';
@@ -30,7 +32,7 @@ export default function Layout(props) {
 
   // user is not logged in, only show landing page
   return (
-    <div>
+    <div className="landing-page">
       <Head_ title="Hagtag" />
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
@@ -40,9 +42,25 @@ export default function Layout(props) {
 
       <Menu />
 
-      <div className="container">
-        <h1>About Us</h1>
-        <p>h</p>
+      <div className="header-container">
+        <h1 className="title">HagTag</h1>
+        <p className="about">An app designed for productivity</p>
+
+        <Image
+          src="/../public/images/hagtaglogo.png"
+          alt="HagTag logo"
+          width="250"
+          height="250"
+        />
+      </div>
+
+      <div className="mid-container">
+        <h1>Features</h1>
+        <p>Calendar</p>
+        <p>Reminder list</p>
+
+        <h1>Name Origin</h1>
+        <p>Hackathon → HagTag</p>
       </div>
 
       <Footer />
