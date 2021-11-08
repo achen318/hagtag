24c2eff4ae63c93b1682a2b266b46994ad4800c0
sudiptacc <sudiptacc@users.noreply.github.com>
2021-11-07 19:15:21 -0500

    fixed it

--- components/widgets/Calendar.js
+++ components/widgets/Calendar.js
@@ -110,7 +110,7 @@ class Calendar extends React.Component {
                 <tr className={styles.calendar} key={i}>
                   {week.map((day, j) => {
                     return <td key={j}>
-                    <button type="button" className="btn btn-white d-inline">🔥</button>
+                    <button type="button" className="btn btn-white d-inline">{day}</button>
                     </td>;
                   })}
                 </tr>
